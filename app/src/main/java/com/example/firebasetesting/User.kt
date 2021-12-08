e169c2883cf19727c38c15fbcaa8b802d256ed34
Alexander Lehm <A.R.Lehm2@ncl.ac.uk>
2021-12-08 23:56:43 +0000

    Add role field to user, implement in LoginActivity and reformat code

--- app/src/main/java/com/example/firebasetesting/User.kt
+++ app/src/main/java/com/example/firebasetesting/User.kt
@@ -2,7 +2,15 @@ package com.example.firebasetesting
 
 import java.io.Serializable
 
-data class User(var name: String, var email: String) : Serializable {
-    constructor() : this  ("", "")
+/**
+ * Data Class to model a User and their fields - password handled by Firebase, no need for storing.
+ * Serializable to allow for passing via Intent
+ *
+ * @param[name] Name of the user
+ * @param[email] Age of the user
+ * @param[role] Role of the user (usually 'user')
+ */
+data class User(var name: String, var email: String, var role: String) : Serializable {
+    constructor() : this("", "", "")
 
 }
