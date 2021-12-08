3ab2bcbba42d1cf51e6c8ac21d53b5a771994e53
Alexander Lehm <A.R.Lehm2@ncl.ac.uk>
2021-12-08 23:49:37 +0000

    Create access check file for RBAC (combine with validation file later)

---
+++ app/src/main/java/com/example/firebasetesting/AccessCheckers.kt
@@ -0,0 +1,13 @@
+package com.example.firebasetesting
+
+object AccessCheckers : IAccessCheckers {
+
+    override fun isUser(user: User): Boolean {
+        return (user.role == Roles.USER.toString())
+    }
+
+    override fun isAdmin(user: User): Boolean {
+        return (user.role == Roles.ADMIN.toString())
+    }
+
+}
---
+++ app/src/main/java/com/example/firebasetesting/IAccessCheckers.kt
@@ -0,0 +1,8 @@
+package com.example.firebasetesting
+
+interface IAccessCheckers {
+
+    fun isUser(user: User): Boolean
+    fun isAdmin(user: User): Boolean
+
+}
